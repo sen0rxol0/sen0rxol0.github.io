2179d8ca8ccecc1f1011785c632557eab2fd2eb4
sen0rxol0 <walter.rony265@gmail.com>
2016-02-27 13:26:56 +0100

    changes made

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,7 +8,7 @@
 
             var $href = $anchorLink.eq(i).attr("href");
 
-            if ( $href === window.location.pathname ) {
+            if ( $href === window.location.href ) {
 
                     $("a.active_page").removeClass("active_page");
 
@@ -64,24 +64,28 @@
         
         var $windowScroll = $(window);
         
-        $(window).on("scroll", function() {
-            
-            //console.log($mainHeader.height());
-            //console.log($(window).scrollTop());
-            if ( $windowScroll.scrollTop() >= 120 ) {
-                
-                $mainHeader.addClass("hide_header");
-                
-                $("#footer_container .menu_active").removeClass("menu_active");
-                $("nav.menu_active").removeClass("menu_active");
-                $("article.menu_active").removeClass("menu_active");
-            } else {
-                
-                $mainHeader.removeClass("hide_header");
-                
-            }
-            
-        });
+        if ( $(document).width() <= 950 ) {
+        
+            $(window).on("scroll", function() {
+
+                //console.log($mainHeader.height());
+                //console.log($(window).scrollTop());
+                if ( $windowScroll.scrollTop() >= 120 ) {
+
+                    $mainHeader.addClass("hide_header");
+
+                    $("#footer_container .menu_active").removeClass("menu_active");
+                    $("nav.menu_active").removeClass("menu_active");
+                    $("article.menu_active").removeClass("menu_active");
+                    
+                } else {
+
+                    $mainHeader.removeClass("hide_header");
+
+                }
+
+            });
+        }
     
         
     }
