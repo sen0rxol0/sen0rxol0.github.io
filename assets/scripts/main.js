a32c6a26eb77fba050ca552b0e93281515bb3955
sen0rxol0 <walter.rony265@gmail.com>
2016-05-21 23:30:47 +0200

    Replaced jquery for vanilla javascript

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,108 +1,61 @@
-(function ($) {
+"use strict";
 
-    function addRemoveClass() {
+function showActivePage() {
+	const anchorEl = document.querySelectorAll("#site_nav a");
+	
+	for (let i = 0; i < anchorEl.length; i++) {
+		
+		let hrefAttr = anchorEl[i].getAttribute("href");
+		if (hrefAttr === window.location.href) {
+			anchorEl[i].classList.add("active_page");
+		}
+	}
+	
+	const activePage = document.querySelector(".active_page");
+	if (!activePage) {
+		anchorEl[0].classList.add("active_page");
+	}
+}
 
-        var $anchorLink = $("#site_nav a");
+function handleMenuToggle() {
+	const menuIcon = document.querySelector(".menu_icon"),
+		  contactLinks = document.querySelector(".footer_links"),
+		  siteNav = document.querySelector("#site_nav"),
+		  mainArticle = document.querySelector(".main_article"),
+		  articlesWrap = document.querySelector("#articles_wrapper"),
+		  aboutArticle = document.querySelector("#about_article");
+	
+	menuIcon.addEventListener("click", () => {
+		menuIcon.classList.toggle("menu_active");
+		contactLinks.classList.toggle("menu_active");
+		siteNav.classList.toggle("menu_active");
+		
+		if (document.body.clientWidth <= 520) {
+			mainArticle.classList.toggle("menu_active");
+			articleWrap.classList.toggle("menu_active");
+			aboutArticle.classList.toggle("menu_active");
+		}
+	});
+	
+}
 
-        $anchorLink.each( function(i) {
+function showHideHeader() {
+	const mainHeader = document.getElementById("main_header");
+	if (document.body.clientWidth <= 950) {
+		window.onscroll = () => {
+			if (window.scrollY >= 120) {
+				mainHeader.classList.add("hide_header");
+				document.querySelector(".menu_icon").classList.remove("menu_active");
+				document.querySelector("div.footer_links").classList.remove("menu_active");
+				document.getElementById("site_nav").classList.remove("menu_active");
+			} else {
+				mainHeader.classList.remove("hide_header");
+			}
+		}
+	}
+	
+}
 
-            var $href = $anchorLink.eq(i).attr("href");
-
-            if ( $href === window.location.href ) {
-
-                    $("a.active_page").removeClass("active_page");
-
-                    $anchorLink.eq(i).addClass("active_page");
-
-             } 
-        });
-        
-        
-        if ( !$anchorLink.hasClass("active_page") ) {
-            
-            $anchorLink.eq(0).addClass("active_page");
-        }
-        
-       
-    }
-    
-    
-    
-    function toggleMenu() {
-        
-        var $menuIcon     = $(".menu_icon"),
-            $contactLinks = $(".footer_links"),
-            $siteNav      = $("#site_nav"),
-            $mainArticle  = $(".main_article"),
-            $articlesWrap = $("#articles_wrapper"),
-            $aboutArticle = $("#about_article");
-    
-        
-        $menuIcon.on("click", function() {
-            
-            $menuIcon.toggleClass("menu_active");
-            $contactLinks.toggleClass("menu_active");
-            $siteNav.toggleClass("menu_active");
-            
-            if ( $(document).width() <= 520 ) {
-                
-                $mainArticle.toggleClass("menu_active");
-                $articlesWrap.toggleClass("menu_active");
-                $aboutArticle.toggleClass("menu_active");
-                
-
-            }
-            
-        });
-        
-    }
-    
-    
-    function showHideHeader() {
-        
-        var $mainHeader = $("#main_header");
-        
-        var $windowScroll = $(window);
-        
-        if ( $(document).width() <= 950 ) {
-        
-            $(window).on("scroll", function() {
-
-                //console.log($mainHeader.height());
-                //console.log($(window).scrollTop());
-                if ( $windowScroll.scrollTop() >= 120 ) {
-
-                    $mainHeader.addClass("hide_header");
-
-                    $("#footer_container .menu_active").removeClass("menu_active");
-                    $("nav.menu_active").removeClass("menu_active");
-                    $("article.menu_active").removeClass("menu_active");
-                    
-                } else {
-
-                    $mainHeader.removeClass("hide_header");
-
-                }
-
-            });
-        }
-    
-        
-    }
-
-    
-    addRemoveClass();
-    toggleMenu();
-    showHideHeader();
-    
-}) (jQuery); // end of the immediately-invoked anonymous fn
-
-
-
-
-
-
-
-
-
-
+showActivePage();
+handleMenuToggle();
+showHideHeader();
